94e2524480cd0935dc87db52729abc2a54af26a5
Sungho Shin <sushin@mit.edu>
2026-04-05 22:16:55 -0400

    partial inline

--- src/wood.jl
+++ src/wood.jl
@@ -2,9 +2,9 @@
 # Institute of Computer Science, Academy of Sciences of the Czech Republic. Technical report No. 767 Problem 5.2
 
 function wood_model end
-wood_start(i) = iseven(i) ? 0.0 : -2.0
+@inline wood_start(i) = iseven(i) ? 0.0 : -2.0
 wood_constraint(x, k) = (2 + 5 * x[k+5]^2) * x[k+5] + 1
 wood_constraint_aug(x, k) = x[k] * (1 + x[k]) + x[k+1] * (1 + x[k+1])
-@inline wood_objective(x, i) = 100 * (x[2i-1]^2 - x[2i])^2 + (x[2i-1] - 1)^2 +
+wood_objective(x, i) = 100 * (x[2i-1]^2 - x[2i])^2 + (x[2i-1] - 1)^2 +
                         90  * (x[2i+1]^2 - x[2i+2])^2 + (x[2i+1] - 1)^2 +
                         10  * (x[2i] + x[2i+2] - 2)^2 + (x[2i] - x[2i+2])^2 / 10
